551e484cf85e13268a98292e37f3fc8f636b4947
permalinku <permalinku@gmail.com>
2025-10-01 17:54:21 -0300

    Shop item WIP

---
+++ Assets/Scripts/ShopItem.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Shop/Item")]
+public class ShopItem : ScriptableObject
+{
+    // the item name
+    public string itemName;
+
+    // the item icon
+    public Sprite icon;
+
+    // the item description
+    [TextArea] 
+    public string description;
+
+    // the item price
+    public int price;
+}
